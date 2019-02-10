f36e0d27faade8e796cc9f82c3f03ce661e1144b
Wanisa Buayam <bywanisa@gmail.com>
2019-02-10 12:31:57 +0700

    Wanisa new port v1 fix

--- src/js/script.js
+++ src/js/script.js
@@ -1,22 +1,16 @@
 $(function(){
 	"use strict";
 
-	/*=========================================================================
-		Initializing stellar.js Plugin
-	=========================================================================*/
+	// Initializing stellar.js Plugin
 	$('.section').stellar({
 		horizontalScrolling: false
 	});
-	
-	
+		
 	$(window).on('load', function(){
 	
 		$('body').addClass('loaded');
-	
 		
-		/*=========================================================================
-			Portfolio Grid
-		=========================================================================*/
+		// Portfolio Grid
 		var grid = $('#portfolio-grid');
 		grid.shuffle({
 			itemSelector: '.item'
@@ -41,11 +35,7 @@ $(function(){
 	
 	});
 	
-	
-	
-	/*=========================================================================
-		Links Navigation System
-	=========================================================================*/
+	// Links Navigation System
 	$('.front-person-links > ul > li > a[data-section]').on('click', function(e){
 		e.preventDefault();
 		var section = $('#' + $(this).data('section'));
@@ -62,117 +52,14 @@ $(function(){
 	$('.close-btn').on('click', function(){
 		$('body').removeClass('section-show');
 		$('section.active').removeClass('active');
-	});
+	});	
 	
-	
-	
-	/*=========================================================================
-		Testimonials Slider
-	=========================================================================*/
-	$('.testimonials-slider').owlCarousel({
-		singleItem: true
-	});
-	
-	
-	
-	/*=========================================================================
-		Skill Bar's Percent Initialization from attribute data-percent
-	=========================================================================*/
+	// Skill Bar's Percent Initialization from attribute data-percent
 	$('.skill-bar').each(function(){
 		var $this = $(this),
 			percent = parseInt( $this.data('percent'), 10 );
 		
 		$this.find('.bar').css('width', percent + '%');
 	});
-	
-	
-	
-	
-	/*=========================================================================
-		Contact Form
-	=========================================================================*/
-	function isJSON(val){
-		var str = val.replace(/\\./g, '@').replace(/"[^"\\\n\r]*"/g, '');
-		return (/^[,:{}\[\]0-9.\-+Eaeflnr-u \n\r\t]*$/).test(str);
-	}
-	$('#contact-form').validator().on('submit', function (e) {
-		
-		if (!e.isDefaultPrevented()) {
-			// If there is no any error in validation then send the message
-			
-			e.preventDefault();
-			var $this = $(this),
-				
-				//You can edit alerts here
-				alerts = {
-				
-					success: 
-					"<div class='form-group' >\
-						<div class='alert alert-success alert-dismissible' role='alert'> \
-							<button type='button' class='close' data-dismiss='alert' aria-label='Close' > \
-								<i class='ion-ios-close-empty' ></i> \
-							</button> \
-							<strong>Message Sent!</strong> We'll be in touch as soon as possible\
-						</div>\
-					</div>",
-					
-					
-					error: 
-					"<div class='form-group' >\
-						<div class='alert alert-danger alert-dismissible' role='alert'> \
-							<button type='button' class='close' data-dismiss='alert' aria-label='Close' > \
-								<i class='ion-ios-close-empty' ></i> \
-							</button> \
-							<strong>Error!</strong> Sorry, an error occurred. Try again.\
-						</div>\
-					</div>"
-					
-				};
-			
-			$.ajax({
-			
-				url: 'mail.php',
-				type: 'post',
-				data: $this.serialize(),
-				success: function(data){
-					
-					if( isJSON(data) ){
-						
-						data = $.parseJSON(data);
-						
-						if(data['error'] == false){
-							
-							$('#contact-form-result').html(alerts.success);
-							
-							$('#contact-form').trigger('reset');
-							
-						}else{
-							
-							$('#contact-form-result').html(
-							"<div class='form-group' >\
-								<div class='alert alert-danger alert-dismissible' role='alert'> \
-									<button type='button' class='close' data-dismiss='alert' aria-label='Close' > \
-										<i class='ion-ios-close-empty' ></i> \
-									</button> \
-									"+ data['error'] +"\
-								</div>\
-							</div>"
-							);
-							
-						}
-						
-						
-					}else{
-						$('#contact-form-result').html(alerts.error);
-					}
-					
-				},
-				error: function(){
-					$('#contact-form-result').html(alerts.error);
-				}
-			});
-		}
-	});
-	
 	
 });
